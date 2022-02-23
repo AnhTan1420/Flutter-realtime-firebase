bcadeed4470dd6fa208a4749ed10cf6e359bb3fd
minh chiu <minh.mchiu@gmail.com>
2022-02-24 06:42:28 +0700

    Design Home Screens and get users from firebase

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,19 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:chat_app/Authenticate/Methods.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class HomeScreen extends StatefulWidget {
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+
+class _HomeScreenState extends State<HomeScreen> {
+  Map<String, dynamic>? userMap;
+  bool isLoading = false;
+  final TextEditingController _search = TextEditingController();
+
+  void onSearch() async {
+    FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+    setState(() {
+      isLoading = true;
+    });
+    await _firestore
+      .collection('users')
+      .where("email", isEqualTo: _search.text)
+      .get()
+      .then((value) {
+      setState(() {
+        userMap = value.docs[0].data();
+        isLoading = false;
+      });
+      print(userMap);
+    });
+  }
   @override
   Widget build (BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Screen")
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () => logOut(),
-          child: Text("LogOut"),
-        ),
-      ),
+      body: isLoading
+            ? Center(
+              child: Container(
+                height: size.height / 20,
+                width: size.height / 20,
+                child: CircularProgressIndicator(),
+              ),
+            )
+            : Column(
+              children: [
+                SizedBox(
+                  height: size.height / 20,
+                ),
+                Container(
+                  height: size.height / 14,
+                  width: size.width,
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: size.height / 14,
+                    width: size.width / 1.15,
+                    child: TextField(
+                      controller: _search,
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: size.height / 50,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Search"),
+                ),
+                SizedBox(
+                  height: size.height / 30,
+                ),
+              ],
+            ),
     );
   }
 }
