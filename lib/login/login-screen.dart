f9e4f54e46afca274449829c13a095d3502c0f2b
sonht113 <trongson1810999@gmail.com>
2022-02-24 08:28:09 +0700

    [hotrongson] finish login-screen

--- lib/login/login-screen.dart
+++ lib/login/login-screen.dart
@@ -131,10 +131,24 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     )
-
-
   }
-
+  Widget field(Size size, String hintText, IconData icon, TextEditingController cont) {
+    return Container(
+      height: size.height / 14,
+      width: size.width / 1.1,
+      child: TextField(
+        controller: cont,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon),
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
 
 
 }
